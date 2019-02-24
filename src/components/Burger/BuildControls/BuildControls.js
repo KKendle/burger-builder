c8306eb4b0d846de210f01345f834809c95b0ae6
Kevin Kendle <kevin.kendle09@gmail.com>
2019-02-24 14:23:17 -0600

    start of adding modal

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './BuildControls.scss';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'},
+    { label: 'Bacon', type: 'bacon'}
+];
+
+// iterate over each control
+// use the labl for the key and also the text label
+// assign and return it to an individual BuildControl component
+const buildControls = (props) => (
+    <div className="BuildControls">
+        <p>Current Price: ${props.price.toFixed(2)}</p>
+        {controls.map(ctrl => {
+            return <BuildControl 
+                        key={ctrl.label}
+                        ingredientLabel={ctrl.label}
+                        added={() => props.ingredientAdded(ctrl.type)}
+                        removed={() => props.ingredientRemoved(ctrl.type)}
+                        disabled={props.disabled[ctrl.type]}
+                    />
+        })}
+        <button
+            className="OrderButton"
+            disabled={!props.purchaseable}>Order Now</button>
+    </div>
+);
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,12 +9,19 @@ const burger = (props) => {
     // (the 'cheese' key has a value of '2' so it creates an array with a length of 2)
     // iterate again, generating a unique key since there are multiple components of the same type
     // return the component with the related type
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>;
+    }
 
     return (
         <div className="Burger">
